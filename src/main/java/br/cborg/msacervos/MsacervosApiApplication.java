1bde55dd6105c0c6c7802bfb00322d263206919d
Rafael Borges <raf.vbs90@gmail.com>
2023-07-13 16:23:36 -0300

    feat:project estructure - initial commit

---
+++ src/main/java/br/cborg/msacervos/MsacervosApiApplication.java
@@ -0,0 +1,13 @@
+package br.cborg.msacervos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsacervosApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsacervosApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/cborg/msacervos/MsacervosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.cborg.msacervos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsacervosApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
